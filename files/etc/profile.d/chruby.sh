662c6d55a4975fe88de0d9c49b810b5e4f5fcd97
Matt Palmer <mpalmer@hezmatt.org>
2013-12-18 16:59:53 +1100

    Need set our RUBIES after chruby.sh does, otherwise it'll all go to hell

--- files/etc/profile.d/chruby.sh
+++ files/etc/profile.d/chruby.sh
@@ -4,6 +4,6 @@
 
 [ -n \"\$BASH_VERSION\" ] || [ -n \"\$ZSH_VERSION\" ] || return
 
-export RUBIES=(/usr/local/lib/rubies/*)
-
 source /usr/local/share/chruby/chruby.sh
+
+export RUBIES=(/usr/local/lib/rubies/*)
